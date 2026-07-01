18ddbb3c865b156b44921649264453bf96660124
Natalia <nataliabaranska023@gmail.com>
2026-07-01 10:15:16 +0200

    Refactor top.sv with debug markers and cleanup
    Added debug markers to various logic signals and cleaned up unused FIFO code.

--- top.sv
+++ top.sv
@@ -8,14 +8,14 @@ module top (
     input  logic start, 
     output logic tx,
 
-    inout  wire SDA,
-    inout  wire SCL
+    inout wire SDA,
+    inout wire SCL
 );
 
 //================ NEURAL CORE =================
 DATA sample_out;
 (* mark_debug = "true" *) logic sample_send;
-logic uart_busy_neur_core;
+(* mark_debug = "true" *) logic uart_busy_neur_core;
 //(* mark_debug = "true" *) 
 
 
@@ -32,44 +32,11 @@ neural_core neur_calc(
     .clk(CLK100MHZ),
     .rst(rst),
     .start(start),
-    .pause(uart_busy_neur_core), //f_full
+    .pause(uart_busy_neur_core), //uart_busy_neur_core), //f_full
     .sample_send(sample_send),
     .sample_out(sample_out)    
 );
 
-
-//================ FIFO =================
-// DATA fifo_rd_data; 
-// logic f_empty, f_full;
-// logic f_wr_en, f_rd_en;
-
-// DATA f_wr_data;
-
-// // zapis do FIFO
-// always_ff @(posedge CLK100MHZ) begin
-//     if (rst) begin
-//         f_wr_en   <= 0;
-//         f_wr_data <= '0;
-//     end else begin
-//         f_wr_en <= 0; // default
-
-//         if (sample_send && !f_full) begin
-//             f_wr_en   <= 1;
-//             f_wr_data <= sample_out;
-//         end
-//     end
-// end
-
-// fifo fifo(
-//     .clk(CLK100MHZ),
-//     .rst(rst),
-//     .wr_en(f_wr_en),
-//     .wr_data(f_wr_data),
-//     .rd_en(f_rd_en),
-//     .rd_data(fifo_rd_data), 
-//     .full(f_full),
-//     .empty(f_empty)
-// );
 
 //================ UART =================
 logic [7:0] u_data;
@@ -116,7 +83,7 @@ generate
     end
 endgenerate
 
-assign uart_busy_neur_core = (state != IDLE) || sample_send || (dac_state != DAC_IDLE);; // when state == idle -> urat = 0, is not busy
+assign uart_busy_neur_core = (state != IDLE) || sample_send; // when state == idle -> urat = 0, is not busy
 
 always_ff @(posedge CLK100MHZ) begin
     if (rst) begin
@@ -146,16 +113,6 @@ always_ff @(posedge CLK100MHZ) begin
             state <= SEND_BYTE;
         end
 
-        // READ_FIFO: begin
-        //     state   <= WAIT_DATA;
-        // end
-
-        // WAIT_DATA: begin
-        //     //data_32 <= fifo_rd_data.y0;
-        //     from_neur_core_data <= fifo_rd_data;
-        //     byte_idx <= 0;
-        //     state   <= SEND_BYTE;
-        // end
 
         SEND_BYTE: begin
             //u_data <= data_flat[191 - byte_idx*8]
@@ -190,12 +147,8 @@ end
 
 
 logic [7:0] data_received;
-//from_neur_core_data.y1
-logic signed [11:0] sample_q6_6_dac1;
-logic signed [11:0] sample_q6_6_dac2;
-logic signed [31:0] sample_delayed;
-
-logic [11:0] dac1_data;
+(* mark_debug = "true" *) logic signed [31:0] sample_delayed;
+(* mark_debug = "true" *) logic [11:0] dac1_data;
 logic [15:0] data_send_dac1;
 
 logic [11:0] dac2_data;
@@ -206,8 +159,7 @@ logic [15:0] data_send_dac2;
 (* mark_debug = "true" *) logic [15:0] current_data;
 
 (* mark_debug = "true" *) logic start_i2c;
-//(* dont_touch = "true" *) logic start_i2c;
-(* mark_debug = "true" *) logic i2c_busy;
+//(* mark_debug = "true" *) logic i2c_busy;
 
 typedef enum logic [2:0] {
 
@@ -232,7 +184,8 @@ always_ff @(posedge CLK100MHZ) begin
         sample_delayed <= sample_out.y0; //y(n-1)
 
 end
-logic signed [31:0] dac1_hold, dac2_hold;
+(* mark_debug = "true" *) logic signed [31:0] dac1_hold;
+logic signed [31:0] dac2_hold;
 
 always_ff @(posedge CLK100MHZ) begin
     if(rst) begin
@@ -244,131 +197,190 @@ always_ff @(posedge CLK100MHZ) begin
     end
 end
 
-///for -5;5, 13/64 = 0.203.., (x*13) >>> 6, 64 = 2^6
-// for -2.5;2.5 -> 51/128 -> 2^7 -> (x*51) >>> 7
-// always_comb begin
-//     sample_q6_6_dac1 = (from_neur_core_data.y0) *51 >>> 27; //removing 20 last bits
-//     dac1_data = sample_q6_6_dac1[11:0] + 12'sd2048;
-//     data_send_dac1  = {4'b0100, dac1_data[11:8], dac1_data[7:0]};
-// // end
-
-// // always_comb begin
-//     sample_q6_6_dac2 = (sample_delayed ) * 51 >>> 27; //removing 20 last bits
-//     dac2_data = sample_q6_6_dac2[11:0] + 12'd2048;
-//     data_send_dac2  = {4'b0100, dac2_data[11:8], dac2_data[7:0]};
-// end
-
 logic signed [63:0] shifted_dac1, shifted_dac2;
-logic signed [63:0] dac1_raw, dac2_raw;
+(* mark_debug = "true" *) logic signed [63:0] dac1_raw, dac2_raw;
 
+logic signed [63:0] dac1_hold_hold, dac2_hold_hold;
+//logic [15:0] buff1, buff2;
 
 always_comb begin
-    shifted_dac1 = $signed(dac1_hold) + 64'sd167772160;
-    dac1_raw = (shifted_dac1 * 64'sd819) >>> 26;
+    dac1_hold_hold = $signed(dac1_hold);
+    shifted_dac1 = dac1_hold_hold + 64'sd140929229;
+    dac1_raw = (shifted_dac1 * 64'sd975) >>> 26; //
     dac1_data = (dac1_raw > 4095) ? 12'd4095 : dac1_raw[11:0];
     data_send_dac1 = {4'b0100, dac1_data};
 
-    shifted_dac2 = $signed(dac2_hold) + 64'sd167772160;
-    dac2_raw = (shifted_dac2 * 64'sd819) >>> 26;
+//-2.1, 2.1
+    dac2_hold_hold = $signed(dac2_hold);
+    shifted_dac2 = dac2_hold_hold + 64'sd140929229; //+2.1
+    dac2_raw = (shifted_dac2 * 64'sd975) >>> 26; //4095/ 4.2
     dac2_data = (dac2_raw > 4095) ? 12'd4095 : dac2_raw[11:0];
-    data_send_dac2 = {4'b0100, dac2_data};
+    data_send_dac2 = {4'b0100, dac2_data}; 
+end
+
+
+logic dac_pending;
+
+always_ff @(posedge CLK100MHZ) begin
+    if(rst)
+        dac_pending <= 0;
+    else if(sample_send)
+        dac_pending <= 1;        
+    else if(dac_state == DAC_SEND_1)
+        dac_pending <= 0;       
 end
 
 logic [15:0] delay_counter;
+(* mark_debug = "true" *)  logic [15:0] current_data_dac1_hold;
+logic [15:0] current_data_dac2_hold;
+
+always_ff @(posedge CLK100MHZ) begin
+    if(rst) begin
+        current_data_dac1_hold <= 0;
+        current_data_dac2_hold <= 0;
+    end else if(sample_send) begin
+        current_data_dac1_hold <= data_send_dac1;
+        current_data_dac2_hold <= data_send_dac2;
+    end
+end
 
 always_ff @(posedge CLK100MHZ) begin
     if(rst) begin 
-        dac_state <= DAC_IDLE;
         start_i2c <= 0;
         current_data<=0;
         current_addr<=0;
         delay_counter <= 0;
+//       current_data_dac1_hold<=0;
+//       current_data_dac2_hold<=0;
+        
+        dac_state <= DAC_IDLE;
 
     end else begin
         start_i2c <= 0;
         case(dac_state)
+
         DAC_IDLE: begin
             delay_counter <= 0;
-            if(sample_send) dac_state <= DAC_SEND_1;
+            if(dac_pending && !i2c_busy) begin //&& !i2c_busy
+//                current_data_dac1_hold<=data_send_dac1;
+//                current_data_dac2_hold<=data_send_dac2;
+                dac_state <= DAC_SEND_1;
+           end
         end
         DAC_SEND_1: begin
-            if(!i2c_busy) begin
+                //$display("%t test_dac data=%h", $time, test_dac);
                 current_addr <= 8'hC0;
-                //current_data <= { 8'b0, data_send_dac1};
-
-                // current_data <= '0;
-                // current_data[15:0] <= data_send_dac1;
-                current_data <= data_send_dac1; //{8'b0, 4'b0100, 12'd2048};
+                current_data <= 16'h0BB8; //current_data_dac1_hold; //data_send_dac1;//current_data_dac1_hold;//16'h0BB8; //
                 start_i2c <= 1;
                 dac_state <= DAC_WAIT_1;
-            end
+
         end
         DAC_WAIT_1: begin
+            start_i2c <= 0;
             if(i2c_busy) begin
-                start_i2c <= 0;
                 dac_state <= DAC_WAIT_1_DONE;
+////            end else begin
+////                start_i2c <= 1;
             end
         end
 
         DAC_WAIT_1_DONE: begin
-            if(!i2c_busy) dac_state <= DAC_DELAY;
+            if(!i2c_busy) 
+                dac_state <= DAC_DELAY;
         end
 
         DAC_DELAY: begin
             if(delay_counter < 16'd100) begin
                 delay_counter <= delay_counter + 1;
             end else begin
+                delay_counter <= 0;
                 dac_state <= DAC_SEND_2;
             end
         end 
 
         DAC_SEND_2: begin
-            if(!i2c_busy)begin
                 current_addr <= 8'hC2;
-
-                current_data <= data_send_dac2;//{8'b0, 4'b0100, dac1_data};
-                //current_data <= { 8'b0, data_send_dac2};
+                current_data <= current_data_dac2_hold; //data_send_dac2;//current_data_dac2_hold; //16'h0BB8;//{8'b0, 4'b0100, dac1_data}; //0- 4095, 3.3V
                 start_i2c <= 1;
                 dac_state <= DAC_WAIT_2;
-                
-            end
+
         end
 
         DAC_WAIT_2: begin
+            start_i2c <= 0;
             if(i2c_busy)begin 
-                start_i2c <= 0;
+//                start_i2c <= 0;
                 dac_state <= DAC_WAIT_2_DONE;
+////            end else begin
+////                start_i2c <= 1; 
             end
         end
 
         DAC_WAIT_2_DONE: begin 
-            if(!i2c_busy)dac_state <= DAC_IDLE;
+            if(!i2c_busy) 
+                dac_state <= DAC_IDLE;
         end
     endcase
     end
 end
 
+//primitive input/output buffer
+wire SCL_in, SDA_in;
+wire SCL_tx_top, SDA_tx_top;
+
+`ifdef SYNTHESIS
+    // IOBUF iobuf_scl (.O(SCL_in), .I(1'b0), .T(SCL_tx_top), .IO(SCL));
+    // IOBUF iobuf_sda (.O(SDA_in), .I(1'b0), .T(SDA_tx_top), .IO(SDA));
+
+
+    //https://docs.amd.com/r/en-US/ug974-vivado-ultrascale-libraries/IOBUF
+    IOBUF IOBUF_scl (
+   .O(SCL_in),   // 1-bit output: Buffer output
+   .I(1'b0),   // 1-bit input: Buffer input
+   .IO(SCL), // 1-bit inout: Buffer inout (connect directly to top-level port)
+   .T(SCL_tx_top)    // 1-bit input: 3-state enable input
+);
+    IOBUF IOBUF_scl (
+   .O(SDA_in),   // 1-bit output: Buffer output
+   .I(1'b0),   // 1-bit input: Buffer input
+   .IO(SCL), // 1-bit inout: Buffer inout (connect directly to top-level port)
+   .T(SDA_tx_top)    // 1-bit input: 3-state enable input
+);
+`else
+    assign SCL = SCL_tx_top ? 1'bz : 1'b0;
+    assign SCL_in = SCL;
+    assign SDA = SDA_tx_top ? 1'bz : 1'b0;
+    assign SDA_in = SDA;
+`endif
+
 
 i2c_master i2c_inst (
-
     .clk(CLK100MHZ),
     .rst(~rst),
     .start(start_i2c),
     .dac_addr(current_addr),
     .data_send(current_data),
     .bytes_send(2),
-    .bytes_receive(0), // so data_received is not used 
-    .SDA(SDA),
-    .SCL(SCL),
+    .bytes_receive(0),
+    .SDA(SDA_in), // input
+    .SCL(SCL_in), // input
+    .SCL_tx_out(SCL_tx_top), // output -> IOBUF
+    .SDA_tx_out(SDA_tx_top), // output ->  IOBUF
     .i2c_busy(i2c_busy),
     .data_received(data_received)
-
 );
 
-//(* mark_debug = "true", dont_touch = "true" *) logic rst_i2c_dbg;
+//====== to go through ILA comfortabely, sice mi window is narrow======
+(* mark_debug = "true" *) logic [31:0] sample_cnt;
 
-//always_ff @(posedge CLK100MHZ)
-//    rst_i2c_dbg <= rst;
+always_ff @(posedge CLK100MHZ) begin
+    if(rst)
+        sample_cnt <= 0;
+    else if(sample_send)
+        sample_cnt <= sample_cnt + 1;
+end
 
 
 endmodule
+
+writer.writerow([{data_count:.10f}, {y0/scale:.10f}, {y1/scale:.10f}, {y2/scale:.10f}, {y3/scale:.10f}, {y4/scale:.10f}, dt/100.0])
